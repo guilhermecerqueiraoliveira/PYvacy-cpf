62faa8bbfff4b7f161eb9fe962a27f63598cca03
José Guilherme Cerqueira de Oliveira <guilhermebot43@gmail.com>
2024-12-24 23:38:10 +0000

    Adicionei uma nova função para criar um diretório para novos pdfs.

--- cpf_privacy.py
+++ cpf_privacy.py
@@ -5,25 +5,30 @@ import os
 import sys
 
 def ocultar_cpf(input_pdf):
-    # abrir o arquivo PDF
+    # Criar o diretório "pdfs" se não existir
+    if not os.path.exists("pdfs"):
+        os.makedirs("pdfs")
+
+    # Abrir o arquivo PDF
     with open(input_pdf, 'rb') as file:
         reader = PyPDF2.PdfFileReader(file)
         writer = PyPDF2.PdfFileWriter()
 
+        # Iterar por todas as páginas do PDF
         for page_num in range(reader.numPages):
             page = reader.getPage(page_num)
             text = page.extract_text()
 
-            # ocultar o CPF
+            # Ofuscar o CPF
             cpf_pattern = r'\d{3}\.\d{3}\.\d{3}-\d{2}'
             text_ocultado = re.sub(cpf_pattern, 'XXX.XXX.XXX-XX', text)
 
-            # adicionar a página modificada ao writer
+            # Adicionar a página modificada ao writer
             page.merge_text(text_ocultado)
             writer.addPage(page)
 
-        # salvar o novo arquivo PDF com o nome modificado
-        output_pdf = f"{os.path.splitext(input_pdf)[0]} (CPF OCULTO).pdf"
+        # Salvar o novo arquivo PDF no diretório "pdfs" com o nome modificado
+        output_pdf = f"pdfs/{os.path.splitext(os.path.basename(input_pdf))[0]} (CPF OCULTO).pdf"
         with open(output_pdf, 'wb') as output_file:
             writer.write(output_file)
 
